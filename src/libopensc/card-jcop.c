56d2165c77f2d7ac4288fe1a972e9491d27e901a
Mikael Magnusson <mikma@users.sourceforge.net>
2013-08-28 21:33:13 +0200

    Fix missing parenthesis in conditions

--- src/libopensc/card-jcop.c
+++ src/libopensc/card-jcop.c
@@ -656,9 +656,9 @@ static int jcop_set_security_env(sc_card_t *card,
 	     return SC_ERROR_INVALID_ARGUMENTS;
         }
         apdu.le = 0;
-        if (!env->flags & SC_SEC_ENV_ALG_REF_PRESENT)
+        if (!(env->flags & SC_SEC_ENV_ALG_REF_PRESENT))
 	     return SC_ERROR_INVALID_ARGUMENTS;
-        if (!env->flags & SC_SEC_ENV_FILE_REF_PRESENT)
+        if (!(env->flags & SC_SEC_ENV_FILE_REF_PRESENT))
 	     return SC_ERROR_INVALID_ARGUMENTS;
         if (env->flags & SC_SEC_ENV_KEY_REF_PRESENT) {
 	     if (env->key_ref_len > 1 || env->key_ref[0] != 0)
